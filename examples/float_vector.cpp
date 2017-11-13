ee52ecb087859b94ac2032e1bff92d55be00c564
Andre Mueller <foss@andremueller-online.de>
2017-11-13 13:02:03 +0100

    added args -> vector<float> example

---
+++ examples/float_vector.cpp
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ *
+ * demo program - part of CLIPP (command line interfaces for modern C++)
+ *
+ * released under MIT license
+ *
+ * (c) 2017 André Müller; foss@andremueller-online.de
+ *
+ *****************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include <clipp.h>
+
+
+int main(int argc, char* argv[])
+{
+    using namespace clipp;
+    using std::cout;
+
+    std::vector<float> nums;
+
+    auto cli = required("--data") & numbers("number", nums);
+
+    if(parse(argc, argv, cli)) {
+        cout << "numbers:\n";
+        for(auto n : nums) cout << n << '\n';
+    } else {
+        cout << "Usage:\n" << usage_lines(cli, argv[0]) << '\n';
+    }
+}
